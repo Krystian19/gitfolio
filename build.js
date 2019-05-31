56169be017fa64909f83f100c96573c88707c40d
Jan Guzman <janfrancisco19@gmail.com>
2019-05-30 23:04:11 -0400

    Setting up a different default background image

--- build.js
+++ build.js
@@ -20,7 +20,7 @@ const config = path.join(outDir, 'config.json');
  */
 async function populateCSS({
     theme = 'light',
-    background = 'https://images.unsplash.com/photo-1553748024-d1b27fb3f960?w=500&h=1000&q=80&fit=crop',
+    background = 'https://images.unsplash.com/photo-1505142468610-359e7d316be0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=500&h=1000&q=80&fit=crop',
 } = {}) {
     /* Get the theme the user requests. Defaults to 'light' */
     theme = `dark.css`;
